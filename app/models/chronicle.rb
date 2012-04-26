ac49e7237f8b721706239c464d43a987833265b8
guillaume <gs@dvdpost.be>
2012-04-26 16:34:20 +0200

    upload

--- app/models/chronicle.rb
+++ app/models/chronicle.rb
@@ -1,7 +1,7 @@
 class Chronicle < ActiveRecord::Base
   belongs_to :category, :class_name => 'ChronicleCategory'
   has_many :contents, :class_name => 'ChronicleContent'
-  has_attached_file :cover, :styles => { :small => "237>x237" };
+  has_attached_file :cover, :styles => { :small => "237>x237" },
                             :url  => "http://private.dvdpost.com/images/chronicles/covers/:id/:style/:basename.:extension",
                             :path => "/home/webapps/dvdpostapp/production/shared/uploaded/chronicles/:id/:style/:basename.:extension"
 
