b2b930a55c3fc3d9a580b1a4bd51366fa957d854
guillaume <gs@dvdpost.be>
2012-04-30 16:08:46 +0200

    debug

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -45,6 +45,7 @@ class ContentsController < ApplicationController
   end
 
   def create
+    coder = HTMLEntities.new
     Actor.where("actors_type = 'dvd_norm' and actors_name like '% %'").each do |a|
       name = a.name
       if params[:chronicle_content][:full_description].scan(Regexp.new("#{name}",'i'))
