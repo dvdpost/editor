4f97a80df5f4ac0cea409bd1dd718e2682c3324c
guillaume <gs@dvdpost.be>
2012-06-29 16:31:36 +0200

    image link

--- app/models/news_content.rb
+++ app/models/news_content.rb
@@ -1,6 +1,6 @@
 class NewsContent < ActiveRecord::Base
   belongs_to :news
   has_attached_file :cover, :styles => { :small => "237>x237" },
-                            :url  => "http://private.dvdpost.com/images/news/covers/:id/:style/:basename.:extension",
+                            :url  => "http://private.dvdpost.com/images/news_covers/:id/:style/:basename.:extension",
                             :path => "/home/webapps/dvdpostapp/production/shared/uploaded/news/:id/:style/:basename.:extension"
 end
