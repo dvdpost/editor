81363ca7c2c112e3473382fb5a17c813894fbe2a
guillaume <gs@dvdpost.be>
2012-07-03 10:23:03 +0200

    size

--- app/models/news_content.rb
+++ app/models/news_content.rb
@@ -1,9 +1,9 @@
 class NewsContent < ActiveRecord::Base
   belongs_to :news
-  has_attached_file :cover, :styles => { :small => "237>x237" },
+  has_attached_file :cover, :styles => { :small => "369x245>" },
                             :url  => "http://private.dvdpost.com/images/news_covers/:id/:style/:basename.:extension",
                             :path => "/home/webapps/dvdpostapp/production/shared/uploaded/news/:id/:style/:basename.:extension"
-  has_attached_file :thumbs, :styles => { :small => "180>x127" },
+  has_attached_file :thumbs, :styles => { :small => "180x127>" },
                              :url  => "http://private.dvdpost.com/images/news_thumbs/:id/:style/:basename.:extension",
                              :path => "/home/webapps/dvdpostapp/production/shared/uploaded/news_thumbs/:id/:style/:basename.:extension"
 end
